cff17146cfadefe4b0fcb261918cc9006e212cf7
apple <apple@macdeMacBook.local>
2017-09-09 11:10:46 +0800

    2017年09月09日11:10:41

--- models/dirs.js
+++ models/dirs.js
@@ -5,19 +5,7 @@ exports.getAllDirs = (callback)=>{
         if(err)
             throw err;
         let allDirs = [];
-        // (function iterator(i){
-        //     if(i == files.length){
-        //         console.log(allDirs);
-        //         callback(allDirs);
-        //         return;
-        //     }
-        //     fs.stat('./uploads/'+files[i],(err, stats)=>{
-        //         if(stats.isDirectory()){
-        //             allDirs.push(files[i]);
-        //         }
-        //         iterator(i+1);
-        //     })
-        // })(0);
+        //为什么ES6规则必须加分号才行呢？以后还是加上分号
         (iterator=(i)=>{
             if(i == files.length){
                 console.log(allDirs);
